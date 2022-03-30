a4f32cd4411401fef91145e85a8deb8d7408f613
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-30 11:10:22 +0700

    Setter Dan Getter Default (menyebabkan error StackOverflowError)

---
+++ src/main/kotlin/id/ac/polbeng/yuni/test_kelas/11_EnkapsulasiB.kt
@@ -0,0 +1,33 @@
+package id.ac.polbeng.yuni.test_kelas
+
+
+class EmployeeA(_id: Int, _name: String, _age: Int) {
+    val id: Int = _id
+        get() = field
+    var name: String = _name
+        get(){
+            return field.toUpperCase()
+        }
+        set(value) {
+            field = value
+        }
+    var age: Int = _age
+        get() = field
+        set(value) {
+            field = if(value > 0) value else throw IllegalArgumentException("Age must be greater than zero")
+        }
+}
+fun main() {
+    val emp = EmployeeA(1101, "Jono", 25)
+    println("Id : ${emp.id}, Nama : ${emp.name}, Umur : ${emp.age}")
+    emp.age = -1
+}
+/*
+Terdapat dua keyword asing yaitu value dan field.
+
+Value adalah nama parameter setter.
+Field ataudisebut juga backing fields membantu kita untuk merujuk property didalam method getter dan
+setter. Field ini sangat dibutuhkan, jika kita menggunakan property secara langsung didalam
+method getter dan setter maka kita akan memanggil program secara rekursif dan menyebabkan
+error StackOverflowError.
+ */
