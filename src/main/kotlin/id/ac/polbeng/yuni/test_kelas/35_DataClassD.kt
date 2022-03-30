7ab2e8ef499f1b8df0722c37d8b776d7c2853fa1
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-30 12:31:24 +0700

    Data class toString() method

---
+++ src/main/kotlin/id/ac/polbeng/yuni/test_kelas/35_DataClassD.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.yuni.test_kelas
+
+fun main(){
+    val meri = Student("Meri", 20)
+    println(meri.toString())
+}
+/*
+Data class toString() method
+Fungsi toString() pada data kelas mengembalikan nilai kembalian String yang merupakan
+representasi dari objek.
+ */
